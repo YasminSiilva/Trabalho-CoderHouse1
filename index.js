a3fda37ceddbb7937b359a264e17c839aa436771
Yasmin <145509401+YasminSiilva@users.noreply.github.com>
2024-03-23 13:16:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+           
+//declaração de variavel
+var nome
+var idade
+var sobrenome
+
+ //atribuição
+idade = 20
+sobrenome =  "Silva"
+nome = "Yasmin"
+
+console.log("Mensagem de teste");
+var saudacao = "Olá mundo!"
+console.log(saudacao)
+
+var texto1 = "CODER"
+var texto2 = "HOUSE"
+var texto3 = "  "
+
+var resultado1 = texto1 + texto3 + texto2
+console.log(resultado1)
+
+var nomeInserido = prompt("Insira seu nome")
+console.log(nomeInserido)
+
+alert("Olá, Yasmin!")
+
+ var stringNumero = prompt("Insira um numero")
+ var valorNumerico = parseInt(stringNumero)
+ console.log(valorNumerico)
+ 
+ var entrada = prompt("Insira uma letra")
+ var saida = entrada + " " + "Bons Estudos!"
+  alert(saida)
+
+
+
+
